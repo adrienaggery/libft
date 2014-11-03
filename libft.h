879bbaf43e57959a90a7227be043a16d275a5e3c
Adrien AGGERY <adrien@aggery.com>
2014-11-03 18:44:04 +0100

    libft.h - v2

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:17 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/03 15:40:27 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/03 18:40:46 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 
 #endif
