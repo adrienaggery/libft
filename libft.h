8c4343089544a16b190aa21ad5a7cb33a96f8691
Adrien AGGERY <adrien@aggery.com>
2014-11-03 15:45:09 +0100

    libft.h, memset & bzero

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/03 11:31:17 by aaggery           #+#    #+#             */
+/*   Updated: 2014/11/03 15:40:27 by aaggery          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+
+#endif
