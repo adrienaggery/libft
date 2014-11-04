2cfb70885cc70621c267cca4edf8e5bfd5965fc8
Adrien AGGERY <adrien@aggery.com>
2014-11-04 21:30:22 +0100

    ft_memchr.c update

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 13:16:28 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 17:16:18 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 21:29:32 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned int	i;
-	char			*s_copy;
+	char	*s_copy;
 
 	s_copy = (char *)s;
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
-		if (s_copy[i] == (unsigned char)c)
-			return ((void *)&s_copy[i]);
-		i++;
+		if (*s_copy == (unsigned char)c)
+			return ((void *)s_copy);
+		s_copy++;
 	}
 	return (NULL);
 }
