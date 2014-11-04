796edfd1e49d8331747510c12e6d6e859176e7eb
Adrien AGGERY <adrien@aggery.com>
2014-11-04 20:40:18 +0100

    ft_memset.c update

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:36:27 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 17:16:40 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 20:39:42 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t		i;
-	char		*b_copy;
+	unsigned char	*b_copy;
 
 	b_copy = b;
-	i = 0;
-	while (i < len)
-	{
-		b_copy[i] = (unsigned char)c;
-		i++;
-	}
+	while (len--)
+		*b_copy++ = (unsigned char)c;
 	return (b);
 }
