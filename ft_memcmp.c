2280878819ed1fed4b051ebf4e5a09126e89b9f7
Adrien AGGERY <adrien@aggery.com>
2014-11-04 21:39:19 +0100

    ft_memcmp.c update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 14:39:11 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 17:10:33 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 21:38:53 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int		i;
 	unsigned char		*s1_copy;
 	unsigned char		*s2_copy;
 
 	s1_copy = (unsigned char *)s1;
 	s2_copy = (unsigned char *)s2;
-	i = 0;
-	while (s1_copy[i] == s2_copy[i] && i < n)
-		i++;
-	return (s1_copy[i] - s2_copy[i]);
+	while (n--)
+	{
+		if (*s1_copy != *s2_copy)
+			return (*s1_copy - *s2_copy);
+		s1_copy++;
+		s2_copy++;
+	}
+	return (0);
 }
