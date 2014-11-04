ddd8dc39322e37d59fe8c0950ebc54e6097b7c80
Adrien AGGERY <adrien@aggery.com>
2014-11-04 16:26:27 +0100

    ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 15:38:26 by aaggery           #+#    #+#             */
+/*   Updated: 2014/11/04 16:25:17 by aaggery          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	int			i;
+	size_t		slen;
+	char		*s1_copy;
+
+	slen = ft_strlen(s1);
+	s1_copy = (char *)malloc(sizeof(char) * (slen + 1));
+	i = 0;
+	while (s1[i])
+	{
+		s1_copy[i] = s1[i];
+		i++;
+	}
+	s1_copy[i] = '\0';
+	return (s1_copy);
+}
