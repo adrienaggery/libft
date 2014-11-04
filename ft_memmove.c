0970034bdc89b817d75e57c105028639f8ff3ebe
Adrien AGGERY <adrien@aggery.com>
2014-11-04 20:25:58 +0100

    ft_memmove.c update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 12:39:47 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 17:16:28 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 20:25:13 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned int	i;
-	char			*dst_copy;
-	char			*src_copy;
+	char	*dst_copy;
+	char	*src_copy;
 
 	dst_copy = (char *)dst;
 	src_copy = (char *)src;
-	i = 0;
-	while (i < len)
+	if (src_copy <= dst_copy)
 	{
-		dst_copy[i] = src_copy[i];
-		i++;
+		dst_copy += len - 1;
+		src_copy += len - 1;
+		while (len--)
+			*dst_copy-- = *src_copy--;
 	}
-	return (dst);
+		else
+	{
+		ft_memcpy(dst, src, len);
+	}
+		return (dst);
 }
