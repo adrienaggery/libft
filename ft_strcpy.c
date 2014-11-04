865a21a095484e146d734e2e953cdc34e1f8f167
Adrien AGGERY <adrien@aggery.com>
2014-11-04 18:23:14 +0100

    ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 16:34:53 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 16:47:40 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 18:16:32 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	
+	memmove(dst, src, ft_strlen(src));
+	dst[ft_strlen(src)] = '\0';
+	return (dst);
 }
