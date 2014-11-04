78e459893d4986b1dcd2ef39974d197c16a3b3db
Adrien AGGERY <adrien@aggery.com>
2014-11-04 20:05:32 +0100

    ft_memcpy.c update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 17:15:33 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/04 16:56:35 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/04 20:03:50 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		i;
-	char		*dst_copy;
-	char		*src_copy;
+	char	*dst_copy;
+	char	*src_copy;
 
 	dst_copy = (char *)dst;
 	src_copy = (char *)src;
-	i = 0;
-	while (i < n)
-	{
-		dst_copy[i] = src_copy[i];
-		i++;
-	}
+	while (n--)
+		*dst_copy++ = *src_copy++;
 	return (dst);
 }
