3215af8bde66ad6fd3a3caf6575a8b2e0cbf79f8
Adrien AGGERY <adrien@aggery.com>
2014-11-05 20:55:44 +0100

    libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:17 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/05 17:03:08 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/05 20:27:51 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,8 @@ char		*ft_strncat(char *s1, const char *s2, size_t n);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
+char		*ft_strstr(const char *s1, const char *s2);
+char		*ft_strnstr(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
 
 #endif
