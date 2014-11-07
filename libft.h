9a4c78acb259fa518984ce4053f1ff82197b673d
Adrien AGGERY <adrien@aggery.com>
2014-11-07 13:24:27 +0100

    new files && libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:17 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/07 12:42:31 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/07 13:23:59 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,7 @@ int			ft_toupper(int c);
 int			ft_tolower(int c);
 
 void		*ft_memalloc(size_t size);
+void		ft_memdel(void **ap);
+char		*ft_strnew(size_t size);
 
 #endif
