55a0e27ccca309e692c8a48d7713d43cbe64384b
Adrien AGGERY <adrien@aggery.com>
2014-11-07 12:26:27 +0100

    ft_atoi.c new

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 15:06:22 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/06 15:35:33 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/07 12:26:06 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 
 int		ft_atoi(const char *str)
 {
-	(void)str;
-	return (0);
+	int		signe;
+	int		result;
+
+	signe = 1;
+	result = 0;
+	while ((*str >= 8 && *str <= 14) || *str == ' ')
+		str++;
+	if ((*str < '0' && *str > '9') && *str != '+' && *str != '-')
+		return (0);
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			signe = -1;
+		str++;
+	}
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		result *= 10;
+		result += *str - 48;
+		str++;
+	}
+	return (result * signe);
 }
