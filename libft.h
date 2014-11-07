d89ad7ac61878456681a1a2843407567a086e66c
Adrien AGGERY <adrien@aggery.com>
2014-11-07 20:35:21 +0100

    ft_strequ.c new, libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:17 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/07 19:36:33 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/07 20:34:52 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,6 @@ void		ft_striter(char *s, void (*f)(char *));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_strmap(char const *s, char (*f)(char));
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int			ft_strequ(char const *s1, char const *s2);
 
 #endif
