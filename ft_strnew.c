3ab332daa9f298cf21d807a8a38e799861c62739
Adrien AGGERY <adrien@aggery.com>
2014-11-09 22:29:26 +0100

    ft_strnew.c cast malloc

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 13:07:40 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/07 13:20:20 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/09 22:29:09 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strnew(size_t size)
 {
 	char	*s;
 
-	s = malloc(sizeof(char) * (size + 1));
+	s = (char *)malloc(sizeof(char) * (size + 1));
 	if (!s)
 		return (NULL);
 	ft_bzero(s, size + 1);
