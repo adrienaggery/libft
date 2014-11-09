3ca711467da3c5ab1ed0a2c7446e20b904799570
Adrien AGGERY <adrien@aggery.com>
2014-11-09 20:09:45 +0100

    putnbr update, putnbr_fd new

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/09 19:14:18 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/09 20:08:42 by aaggery          ###   ########.fr       */
+/*   Created: 2014/11/09 20:07:23 by aaggery           #+#    #+#             */
+/*   Updated: 2014/11/09 20:08:58 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int nb)
+void	ft_putnbr_fd(int n, int fd)
 {
+
 	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
@@ -21,9 +22,9 @@ void	ft_putnbr(int nb)
 	}
 	else if (nb < 0)
 	{
-		ft_putchar('-');
+		ft_putchar_fd('-', fd);
 		ft_putnbr(-nb);
 	}
 	else
-		ft_putchar(nb + '0');
+		ft_putchar_fd(nb + '0', fd);
 }
