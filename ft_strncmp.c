803d46387640496ec5a0b00154b2a7996edf82f1
Adrien AGGERY <adrien@aggery.com>
2014-11-11 17:53:34 +0100

    norme strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 11:40:08 by aaggery           #+#    #+#             */
-/*   Updated: 2014/11/11 17:07:13 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/11/11 17:53:20 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-
 	while (n--)
 	{
 		if (!*str1 || *str1 != *str2)
